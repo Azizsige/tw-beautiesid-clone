993b41dfa7f539778644a8f25831298e201a7c83
azizsige <kurniawan00azizsige@gmail.com>
2022-02-23 17:07:21 +0700

    testing hero section part 5

---
+++ dist/js/app.js
@@ -0,0 +1,21 @@
+const toggle = document.getElementById("toggle");
+let changed = document.getElementById("changed");
+let navbar = document.getElementById("mobile-menu-4");
+
+toggle.addEventListener("click", function () {
+  navbar.classList.toggle("block");
+  navbar.classList.toggle("hidden");
+});
+// Create a condition that targets viewports at least 768px wide
+const mediaQuery = window.matchMedia("(min-width: 640px)");
+function handleTabletChange(e) {
+  // Check if the media query is true
+  if (e.matches) return changed.src = "./assets/pixel-pc.png";
+  if(!e.matches) return changed.src = "./assets/pixel-mobile.png"
+  
+}
+// Register event listener
+mediaQuery.addListener(handleTabletChange);
+
+// Initial check
+handleTabletChange(mediaQuery);
