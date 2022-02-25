70c159a05ba00c18c68a332d872e80011ee21871
azizsige <kurniawan00azizsige@gmail.com>
2022-02-25 14:38:25 +0700

    testing responsive section one part 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
         primary: ['"Roboto", "sans-serif"'],
       },
       boxShadow: {
-        'custom': '10px 10px 30px 0px rgba(234, 141, 136, 40%)'
+        'custom': '10px 10px 30px 0px rgba(234, 141, 136, 40%)',
+        'custom2': '16px 16px 60px 0px rgba(178, 94, 90, 60%)'
       }
     },
   },
