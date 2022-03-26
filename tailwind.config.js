33aa46efcba63b1d6a4d7e399a2e38d4c52337f5
azizsige <kurniawan00azizsige@gmail.com>
2022-03-26 18:22:25 +0700

    finish section 2 - testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
       boxShadow: {
         'custom': '10px 10px 30px 0px rgba(234, 141, 136, 40%)',
         'custom2': '16px 16px 60px 0px rgba(178, 94, 90, 60%)'
+      },
+      backgroundImage: {
+        'section-2': "url('/assets/bgsec2.png')",
+      },
+      colors: {
+        'heading': '#555555',
+        'paragraph': '#888888'
       }
     },
   },
