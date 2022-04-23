f876daaf989c1710a6760350c0c0886c79ac264e
Azizsige <kurniawan00azizsige@gmail.com>
2022-04-23 17:06:38 +0700

    update: restyling box shadow in section 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
       boxShadow: {
         'custom': '10px 10px 30px 0px rgba(234, 141, 136, 40%)',
         'custom2': '16px 16px 60px 0px rgba(178, 94, 90, 60%)',
-        'section2': '10px 10px 18px 4px rgba(217,217,217,0.73)'
+        'section2': '10px 10px 18px 4px rgba(217,217,217,0.73)',
+        'section4': '5px 8px 16px -4px rgba(217,217,217,0.73)'
       },
       backgroundImage: {
         'section-2': "url('/assets/bgsec2.png')",
