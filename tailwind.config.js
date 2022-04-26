38ec5fdb85e6f95edce2f1098cd7006ab0d39dff
unknown <kurniawan00azizsige@gmail.com>
2022-04-26 20:13:19 +0700

    update: test website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 module.exports = {
-  content: ["./dist/js/*.js", "./dist/css/*.css", "./*.html"],
+  content: ["./dist/js/*.js", "./dist/css/*.css", "./*.html", "./pages/*.html"],
   theme: {
     extend: {
       fontFamily: {
         primary: ['"Roboto", "sans-serif"'],
       },
       boxShadow: {
-        'custom': '10px 10px 30px 0px rgba(234, 141, 136, 40%)',
-        'custom2': '16px 16px 60px 0px rgba(178, 94, 90, 60%)',
-        'section2': '10px 10px 18px 4px rgba(217,217,217,0.73)',
-        'section4': '5px 8px 16px -4px rgba(217,217,217,0.73)'
+        custom: "10px 10px 30px 0px rgba(234, 141, 136, 40%)",
+        custom2: "16px 16px 60px 0px rgba(178, 94, 90, 60%)",
+        section2: "10px 10px 18px 4px rgba(217,217,217,0.73)",
+        section4: "5px 8px 16px -4px rgba(217,217,217,0.73)",
       },
       backgroundImage: {
-        'section-2': "url('/assets/bgsec2.png')",
+        "section-2": "url('/assets/bgsec2.png')",
       },
       colors: {
-        'heading': '#555555',
-        'paragraph': '#888888',
-        'brand': '#EE9591'
-      }
+        heading: "#555555",
+        paragraph: "#888888",
+        brand: "#EE9591",
+      },
     },
   },
   plugins: [],
